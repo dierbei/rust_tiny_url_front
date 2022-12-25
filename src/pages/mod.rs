6982df5870c6ff355b539c8cf56e5e255a9862fe
xiaolatiao <1628652790@qq.com>
2022-12-25 15:31:22 +0800

    Feat Trunk.toml 增加后端配置 Feat 完成短连接列表

--- src/pages/mod.rs
+++ src/pages/mod.rs
@@ -3,3 +3,13 @@ mod links;
 
 pub use links::Links;
 pub use create::Create;
+
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all="snake_case")]
+struct TinyData<T> {
+    pub data: T,
+    pub ok: bool,
+    pub err: Option<String>,
+}
