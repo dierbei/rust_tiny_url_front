7a109057f434ab29f35cc67a72205678c0884d12
xiaolatiao <1628652790@qq.com>
2022-12-25 10:36:49 +0800

    Feat 集成 yew 框架

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+pub fn app() -> Html {
+    html! {
+        <h1> { "Hello Yew" } </h1>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<App>();
 }
